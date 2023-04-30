ff446b30b8158857441d9147d1e3cd9d8f3c680d
amod.mahajan <amod.mahajan@hotmail.com>
2023-04-29 22:24:08 -0400

    Initial commit

---
+++ src/demo/HellowWorld.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class HellowWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+	public static void printSomething() {
+		System.out.println("Print something");
+	}
+
+}
